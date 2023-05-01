70da6a09842e9838074a8870d1ef414a8e3d0955
sunnytan53 <haoyuan.t@outlool.com>
2023-05-01 13:31:45 -0700

    add select all

--- global_data.py
+++ global_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from dash import dcc, html, callback, Input, Output
+import dash_bootstrap_components as dbc
 
 
 df_final = pd.read_csv(
@@ -57,64 +58,145 @@ def level_component():
                 "Region",
                 inline=True,
                 id="level",
-                className="pt-3 s-5",
+                className="pt-3 fs-5",
                 labelClassName="px-3",
             ),
-            dcc.Dropdown(
-                list(locations.keys()),
-                placeholder="Click to search/select regions",
-                multi=True,
-                id="select-region",
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dcc.Dropdown(
+                            list(locations.keys()),
+                            multi=True,
+                            id="select-region",
+                            placeholder="Click to search, or select all",
+                        )
+                    ),
+                    dbc.Col(dbc.Checkbox(id="all-region", label="All regions"), width=3),
+                ],
                 className="pt-3",
             ),
-            dcc.Dropdown(multi=True, id="select-state", className="pt-3"),
-            dcc.Dropdown(multi=True, id="select-city", className="pt-3"),
-        ],
-        className="justify-content-evenly",
+            dbc.Row(
+                [
+                    dbc.Col(dcc.Dropdown(multi=True, id="select-state")),
+                    dbc.Col(dbc.Checkbox(id="all-state", label="All states"), width=3),
+                ],
+                className="pt-3",
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(dcc.Dropdown(multi=True, id="select-city")),
+                    dbc.Col(dbc.Checkbox(id="all-city", label="All cities"), width=3),
+                ],
+                className="pt-3",
+            ),
+        ]
     )
 
 
 @callback(
     Output("select-state", "placeholder"),
-    Output("select-state", "disabled"),
     Output("select-state", "options"),
     Output("select-state", "value"),
+    Output("select-state", "disabled"),
     Input("level", "value"),
     Input("select-region", "value"),
+    Input("select-region", "options"),
+    Input("all-region", "value"),
+    Input("all-state", "value"),
 )
-def switch_select_state(level: str, values: list):
+def switch_select_state(
+    level: str,
+    values: list,
+    region_options: list,
+    all_region: bool,
+    all_state: bool,
+):
+    ret = ["Select a region first", [], [], True]
     if level == "Region":
-        return "Not state or city level", True, [], []
-
-    if values:
-        arr = []
-        for region in values:
-            for state in locations[region].keys():
-                arr.append(f"{region} - {state}")
-        return "Click to search/select state", False, sorted(arr), []
+        ret[0] = "Not state or city level"
+    else:
+        if all_region:
+            values = region_options
+        if values:
+            for region in values:
+                for state in locations[region].keys():
+                    ret[1].append(f"{region} / {state}")
+            # show states
+            ret[1] = sorted(ret[1])
+            if all_state:
+                ret[3] = True
+                ret[0] = "All states selected"
+            else:
+                ret[3] = False
+                ret[0] = "Click to search/select states"
     # empty selection
-    return "Select a region first", True, [], []
+    return ret
 
 
 @callback(
     Output("select-city", "placeholder"),
-    Output("select-city", "disabled"),
     Output("select-city", "options"),
     Output("select-city", "value"),
+    Output("select-city", "disabled"),
     Input("level", "value"),
     Input("select-state", "value"),
+    Input("select-state", "options"),
+    Input("all-state", "value"),
+    Input("all-city", "value"),
 )
-def switch_select_city(level: str, values: str):
-    if level == "City":
+def switch_select_city(
+    level: str,
+    values: str,
+    state_options: list,
+    all_state: bool,
+    all_city: bool,
+):
+    ret = ["Select a state first", [], [], True]
+    if level != "City":
+        ret[0] = "Not city level"
+    else:
+        if all_state:
+            values = state_options
         if values:
-            arr = []
             for x in values:
-                region, state = x.split(" - ")
+                region, state = x.split(" / ")
                 for city in locations[region][state]:
-                    arr.append(f"{region} - {state} - {city}")
+                    ret[1].append(f"{x} / {city}")
             # show cities
-            return "Click to search/select cities", False, sorted(arr), []
-        # empty selection
-        return "Select a state first", True, [], []
+            ret[1] = sorted(ret[1])
+            if all_city:
+                ret[3] = True
+                ret[0] = "All cities selected"
+            else:
+                ret[3] = False
+                ret[0] = "Click to search/select cities"
+    # empty selection
+    return ret
 
-    return "Not city level", True, [], []
+
+@callback(
+    Output("all-region", "disabled"),
+    Output("all-state", "disabled"),
+    Output("all-city", "disabled"),
+    Input("level", "value"),
+)
+def swtich_all(level: str):
+    arr = [False] * 3
+    if level == "Region":
+        arr[1] = True
+        arr[2] = True
+    elif level == "State":
+        arr[2] = True
+    return arr
+
+
+@callback(
+    Output("select-region", "placeholder"),
+    Output("select-region", "value"),
+    Output("select-region", "disabled"),
+    Input("all-region", "value"),
+)
+def select_all_region(all_region: list):
+    if all_region:
+        return "All regions selected", [], True
+    return "Click to search/select regions", [], False
