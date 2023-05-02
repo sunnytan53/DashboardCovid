1a192f4373c7e832d2cb3d70aedbdb7af63175e8
sunnytan53 <haoyuan.t@outlool.com>
2023-05-01 21:38:26 -0700

    add back sorting

--- dataProcess/data_3_combine.py
+++ dataProcess/data_3_combine.py
@@ -19,21 +19,29 @@ df_in = pd.read_csv(
 print("*** exporting data_region.zip")
 df_in.drop(["State", "City"], axis=1).groupby(
     ["Region", "Date"], as_index=False
-).sum().to_csv("data_region.zip", compression="zip", index=False)
+).sum().sort_values(["Region", "Date"]).to_csv(
+    "data_region.zip", compression="zip", index=False
+)
 
 print("*** exporting data_state.zip")
 df_out = df_in.drop(["City"], axis=1).dropna(subset=["State"])
 df_out["State"] = (
     df_out["Region"].astype(str) + " / " + df_out["State"].astype(str)
 ).astype("category")
-df_out.drop(["Region"], axis=1).groupby(["State", "Date"], as_index=False).sum().to_csv(
+df_out.drop(["Region"], axis=1).groupby(
+    ["State", "Date"], as_index=False
+).sum().sort_values(["State", "Date"]).to_csv(
     "data_state.zip", compression="zip", index=False
 )
 del df_out
 
 print("*** exporting data_city.zip")
 df_out = df_in.dropna(subset=["State", "City"])
-df_out["City"] = df_out["Region"].str.cat([df_out["State"], df_out["City"]], " / ").astype("category")
-df_out.drop(["Region", "State"], axis=1).to_csv(
+df_out["City"] = (
+    df_out["Region"]
+    .str.cat([df_out["State"], df_out["City"]], " / ")
+    .astype("category")
+)
+df_out.drop(["Region", "State"], axis=1).sort_values(["City", "Date"]).to_csv(
     "data_city.zip", compression="zip", index=False
 )
